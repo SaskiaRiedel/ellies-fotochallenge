8555d15d008650fd41d516bd388609277601f63c
SaskiaRiedel <saskia.riedel1988@gmail.com>
2026-03-06 21:55:43 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
   cloudName: "dl5uiaorh",
   uploadPreset: "Geburtstagsparty Ellies",
+
   eventTag: "ellie60",
   eventTitle: "Ellies Fotochallenge",
 
@@ -9,25 +10,25 @@ window.APP_CONFIG = {
       id: "aufgabe1",
       title: "Fotografiere Menschen auf der Tanzfläche",
       caption: "Einfach mal aus der Reihe tanzen!",
-      color: "#F6D6D6",
-      folder: "ellie60/aufgabe1",
-      tags: ["ellie60", "aufgabe1", "fotochallenge"]
+      color: "#f1d1d1",
+      tags: ["ellie60", "fotochallenge", "aufgabe1"],
+      listTag: "aufgabe1"
     },
     {
       id: "aufgabe2",
       title: "Mach ein gemeinsames Foto von der jüngsten und ältesten Person",
       caption: "Altern ist unvermeidlich, erwachsen werden ist optional.",
-      color: "#DCECCB",
-      folder: "ellie60/aufgabe2",
-      tags: ["ellie60", "aufgabe2", "fotochallenge"]
+      color: "#d7e6c5",
+      tags: ["ellie60", "fotochallenge", "aufgabe2"],
+      listTag: "aufgabe2"
     },
     {
       id: "sammelordner",
       title: "Kindheitsbilder und freie Uploads",
       caption: "Erinnerungen aus allen Zeiten",
-      color: "#D9EAF7",
-      folder: "ellie60/sammelordner",
-      tags: ["ellie60", "sammelordner", "fotochallenge"]
+      color: "#d5e6f4",
+      tags: ["ellie60", "fotochallenge", "sammelordner"],
+      listTag: "sammelordner"
     }
   ]
 };
