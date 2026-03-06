69470fbef8fcb8e66d1aafc6c125e0ec4abe3d9f
SaskiaRiedel <saskia.riedel1988@gmail.com>
2026-03-06 22:40:35 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -10,22 +10,19 @@ window.APP_CONFIG = {
       id: "aufgabe1",
       title: "Fotografiere Menschen auf der Tanzfläche",
       caption: "Einfach mal aus der Reihe tanzen!",
-      color: "#f1d1d1",
-      tags: ["ellie60", "fotochallenge", "aufgabe1"]
+      color: "#f1d1d1"
     },
     {
       id: "aufgabe2",
       title: "Mach ein gemeinsames Foto von der jüngsten und ältesten Person",
       caption: "Altern ist unvermeidlich, erwachsen werden ist optional.",
-      color: "#d7e6c5",
-      tags: ["ellie60", "fotochallenge", "aufgabe2"]
+      color: "#d7e6c5"
     },
     {
       id: "sammelordner",
       title: "Kindheitsbilder und freie Uploads",
       caption: "Erinnerungen aus allen Zeiten",
-      color: "#d5e6f4",
-      tags: ["ellie60", "fotochallenge", "sammelordner"]
+      color: "#d5e6f4"
     }
   ]
 };
